19e3f8310fb0d0ea25b4300c99df8bce8b031efe
Dewen <863572313@qq.com>
2025-10-14 16:18:35 +0800

    C++ 指针和函数

---
+++ 8pointer2/pointerFunc.cpp
@@ -0,0 +1,22 @@
+//不带指针的引用传递
+#include <iostream>
+
+using namespace std;
+
+void swap(int &, int &);
+
+// 不带指针的引用传递
+int main() {
+    int a = 10, b = 20;
+    cout << "Before swap, value of a :" << a << " and b :" << b << endl;
+    swap(a, b);
+    cout << "After swap, value of a :" << a << " and b :" << b << endl;
+    return 0;
+}
+
+void swap(int &a, int &b) {
+    int temp;
+    temp = a;
+    a = b;
+    b = temp;
+}
